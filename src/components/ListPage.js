c922554a62ffe46f3b10f60029718ed30beaac7c
zterhes <zterhes1@gmail.com>
2021-01-05 17:49:00 +0100

    1 file modified

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 export default ({ props }) => {
   return (
     <div className="row justify-content-center">
-      <div className="col-4" >
-      <p>
+      <div className="col-4">
+        <p>{props.title}</p>
       </div>
     </div>
   );
